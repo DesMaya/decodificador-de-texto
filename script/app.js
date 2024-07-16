add492be6d4480f3703c161c112d1e004d263307
Maya FLorentino <mayaflorentinodasilva@gmail.com>
2024-07-16 17:55:56 -0300

    logica inicial de criptografar

--- script/app.js
+++ script/app.js
@@ -7,29 +7,54 @@
     "o": "ober",
     "u": "ufat"
  */
+const cifra = {
+    "e": "enter",
+    "i": "imes",
+    "a": "ai",
+    "o": "ober",
+    "u": "ufat"
+};
 
-    // const hasUpper = (str) => /[A-Z]/.test(str);
 const limpaTextarea = () => {
     let textarea = document.getElementById("conteudo-texto");
     textarea.value = "";
 }
 
-function verificaMensagem(){
-    let textarea = document.getElementById("conteudo-texto");
-    let mensagem = textarea.value;
-
-    if (!/[a-z]/.test(mensagem)){
-        alert("Apenas letras minúsculas e sem acento.");
-        return limpaTextarea();
+function verificaMensagem(mensagem){
+    if (!mensagem.length){
+        alert("Digite uma mensagem");
+        return false;
     }
-    else return mensagem;
+    else if (!/[a-z]/.test(mensagem)){
+        alert("Apenas letras minúsculas e sem acento.");
+        limpaTextarea();
+        return false;
+    }
+    else return true;
 
 }
 
 function criptografar() {
+    let textarea = document.getElementById("conteudo-texto");
+    let mensagem = textarea.value;
 
+    if (!verificaMensagem(mensagem)) return;
+    
+
+    const letrasNormais = Object.keys(cifra)
+    const codificadas = Object.values(cifra)
+    //console.log(codificadas)
+
+    letrasNormais.forEach( (letra, i) => {
+        //console.log(letra, codificadas[i])
+        mensagem = mensagem.replaceAll(letra, codificadas[i])
+    })
+    //console.log(mensagem)
+    return mensagem;
 }
 
 function descriptografar() {
 
 }
+
+

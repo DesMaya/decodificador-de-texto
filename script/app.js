9c9fe0dbf423d06fd604e3cae70709702e484033
Maya FLorentino <mayaflorentinodasilva@gmail.com>
2024-07-16 15:50:10 -0300

    adicionando as funçoes, verificaTexto, criptografar e descriptografar

--- script/app.js
+++ script/app.js
@@ -1,3 +1,27 @@
-function criptografar(texto) {
-    let textarea = document.getElementById("input-mensagem");
+/**
+ * Cifra de referência
+ * 
+ * "e": "enter",
+    "i": "imes",
+    "a": "ai",
+    "o": "ober",
+    "u": "ufat"
+ */
+
+function verificaTexto(){
+    let textarea = document.getElementById("conteudo-texto");
+    let mensagem = textarea.value;
+    let mensagemCorrigida;
+
+
+
+    return mensagemCorrigida;
+}
+
+function criptografar(mensagem) {
+
+}
+
+function descriptografar(mensagem) {
+
 }
